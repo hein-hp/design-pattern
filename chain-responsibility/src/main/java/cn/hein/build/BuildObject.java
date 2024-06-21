179ee09c3beed2f9813cb91c51b7ec6ce311da98
hein-hp <hein.hpq@gmail.com>
2024-06-21 19:14:24 +0800

    feature: 实现责任链模式之构建对象

---
+++ chain-responsibility/src/main/java/cn/hein/build/BuildObject.java
@@ -0,0 +1,14 @@
+package cn.hein.build;
+
+/**
+ * 构建对象
+ *
+ * @author hein
+ */
+public class BuildObject {
+
+    public static void main(String[] args) {
+        System.out.println("具体代码查看 code-generate 项目");
+        System.out.println("code address: https://github.com/hein-hp/code-generator");
+    }
+}
